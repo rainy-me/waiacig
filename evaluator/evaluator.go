6642744189758715a2fb82a3525b8516077b1958
rainy-me <github@rainy.me>
2020-09-13 13:46:34 +0900

    handle prefixed eval

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -5,12 +5,33 @@ import (
 	"waiig/object"
 )
 
+var (
+	NULL  = &object.Null{}
+	TRUE  = &object.Boolean{Value: true}
+	FALSE = &object.Boolean{Value: false}
+)
+
 func Eval(node ast.Node) object.Object {
 	switch node := node.(type) {
 	case *ast.Program:
 		return evalStatements(node.Statements)
 	case *ast.ExpressionStatement:
 		return Eval(node.Expression)
+	case *ast.PrefixExpression:
+		right := Eval(node.Right)
+		switch node.Operator {
+		case "!":
+			return evalBangOperatorExpression(right)
+		case "-":
+			return evalMinusPrefixOperatorExpression(right)
+		default:
+			return NULL
+		}
+	case *ast.Boolean:
+		if node.Value {
+			return TRUE
+		}
+		return FALSE
 	case *ast.IntegerLiteral:
 		return &object.Integer{Value: node.Value}
 	}
@@ -23,4 +44,25 @@ func evalStatements(statements []ast.Statement) object.Object {
 		result = Eval(statement)
 	}
 	return result
+}
+
+func evalBangOperatorExpression(right object.Object) object.Object {
+	switch right {
+	case TRUE:
+		return FALSE
+	case FALSE:
+		return TRUE
+	case NULL:
+		return TRUE
+	default:
+		return FALSE
+	}
+}
+
+func evalMinusPrefixOperatorExpression(right object.Object) object.Object {
+	if right.Type() != object.INTEGER_OBJ {
+		return NULL
+	}
+	value := right.(*object.Integer).Value
+	return &object.Integer{Value: -value}
 }
