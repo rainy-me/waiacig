846e7b885d36e7045de555414ee359bf2e3de076
rainy-me <github@rainy.me>
2020-09-21 04:11:18 +0900

    add bulitin puts function

--- evaluator/builtins.go
+++ evaluator/builtins.go
@@ -1,6 +1,7 @@
 package evaluator
 
 import (
+	"fmt"
 	"waiig/object"
 )
 
@@ -86,6 +87,14 @@ var builtins = map[string]*object.Builtin{
 			copy(newElements, arr.Elements)
 			newElements[length] = args[1]
 			return &object.Array{Elements: newElements}
+		},
+	},
+	"puts": &object.Builtin{
+		Fn: func(args ...object.Object) object.Object {
+			for _, arg := range args {
+				fmt.Println(arg.Inspect())
+			}
+			return NULL
 		},
 	},
 }
